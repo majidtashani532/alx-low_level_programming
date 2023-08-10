70da6cc8c9adf3cb08f85de396016f82d12fb499
majidtashani532 <majidtashani532@gmail.com>
2023-08-10 08:46:50 -0700

    Tasks 4 Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+* main - Entry point
+*
+*Description:  a C program that prints with put function
+*
+* Return: Always (0) success
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
